db65d171b3be6e2d0960be392be93cc9724ba981
woo-chia-wei <chiaweiwoo123@gmail.com>
2018-04-16 16:42:57 +0800

    Created test folder with sample of sqlite

---
+++ test/sql.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+connection = sqlite3.connect('data.db')
+
+cursor = connection.cursor()
+
+create_table = "CREATE TABLE users \
+               (id int, \
+                username text, \
+                password text)" 
+
+cursor.execute(create_table)
+
+# Single query
+user = (1, 'jose', 'asdf')
+insert_query = "INSERT INTO users VALUES (?, ?, ?)"
+cursor.execute(insert_query, user) #cursor will fill in the ? fields
+
+# Multiple query
+users = [
+    (2, 'rolf', 'asdf'),
+    (3, 'anne', 'xyz'),
+]
+cursor.executemany(insert_query, users) 
+
+# Retrieve data
+select_query = "SELECT * FROM users"
+for row in cursor.execute(select_query):
+    print(row)
+
+connection.commit()
+
+connection.close()
