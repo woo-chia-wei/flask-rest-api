5ebb38051d0e706831f0ebe567de0b98c5e7f329
woo-chia-wei <chiaweiwoo123@gmail.com>
2018-04-16 16:16:33 +0800

    Minor refactoring on app.py

--- 02_api_auth_no_db/app.py
+++ 02_api_auth_no_db/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
@@ -22,6 +22,13 @@ jwt = JWT(app, authenticate, identity)
 items = []
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help="This field cannot be left blank!"
+    )
+
     @jwt_required() #authenticated before calling
     def get(self, name):
         # Use next get the first item in filtered result
@@ -30,11 +37,12 @@ class Item(Resource):
         return {'item': item}, 200 if item else 404 # http not found
 
     def post(self, name):
+        # Error first approach
         if next(filter(lambda x: x['name'] == name , items), None):
             return {'message': f'An item with name {name} already exists.'}, 400 #http bad request
 
-        data = request.get_json()
-        print('post', data)
+        data = Item.parser.parse_args()
+
         item = {
             'name': name, 
             'price': data['price']
@@ -48,12 +56,16 @@ class Item(Resource):
         return {'message': 'Item deleted'}
 
     def put(self, name):
-        data = request.get_json()
+        data = Item.parser.parse_args()
+        
         item = next(filter(lambda x: x['name'] == name , items), None)
         if item is None:
+            item = {'name': name, 'price':data['price']}
             items.append(item)
         else:
-            item.update(data)
+            # We have some risk here, data might overwrite name
+            # That's why we use parse_args() instead of get_json()
+            item.update(data) 
         return item
 
 class ItemList(Resource):
