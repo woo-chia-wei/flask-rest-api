cfd8c43745e0dd7613aed0076aa803f3fe6e9038
woo-chia-wei <chiaweiwoo123@gmail.com>
2018-04-16 17:09:56 +0800

    Authenticate and get identity from users from db

--- 03_api_auth_sql_db/security.py
+++ 03_api_auth_sql_db/security.py
@@ -5,17 +5,12 @@ users = [
     User(1, 'bob', 'asdf')
 ]
 
-# So that user can be accessed by user name
-username_mapping = {u.username: u for u in users}
-
-# So that user can be accessed by user id
-userid_mapping = {u.id: u for u in users}
 
 def authenticate(username, password):
-    user = username_mapping.get(username, None)
+    user = User.find_by_username(username)
     if user and safe_str_cmp(user.password, password):
         return user
 
 def identity(payload):
     user_id = payload['identity']
-    return userid_mapping.get(user_id, None)
+    return User.find_by_userid(user_id)
--- 03_api_auth_sql_db/user.py
+++ 03_api_auth_sql_db/user.py
@@ -1,5 +1,42 @@
+import sqlite3
+
 class User:
     def __init__(self, _id, username, password): # _id instead of id because id is keyword
         self.id = _id
         self.username = username
         self.password= password
+
+    @classmethod
+    def find_by_username(cls, username):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM users WHERE username=?"
+        result = cursor.execute(query, (username,)) # way to create tuple with single value
+        row = result.fetchone()
+
+        if row:
+            # user = cls(row[0], row[1], row[2])
+            user = cls(*row) # same as above, shorter syntax
+        else:
+            user = None
+
+        connection.close()
+        return user
+    
+    @classmethod
+    def find_by_userid(cls, _id):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM users WHERE id=?"
+        result = cursor.execute(query, (_id,))
+        row = result.fetchone()
+
+        if row:
+            user = cls(*row)
+        else:
+            user = None
+
+        connection.close()
+        return user
